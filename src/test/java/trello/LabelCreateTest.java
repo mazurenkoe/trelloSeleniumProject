da5015acbad913b62a6401463b2915a5cac32c3c
Elena Mazurenko <mazurenko.olena@pdffiller.team>
2019-07-17 03:38:57 +0300

    Exam updates

--- src/test/java/trello/LabelCreateTest.java
+++ src/test/java/trello/LabelCreateTest.java
@@ -12,18 +12,16 @@ import java.util.Date;
 public class LabelCreateTest extends BrowserFactory {
 
     @BeforeMethod
-    public void authorization1() {
+    public void authorization() {
         pages.loginPage.open();
         pages.loginPage.login();
         wait.until(ExpectedConditions.urlContains("/boards"));
         driver.get("https://trello.com/b/EV6lBD3p/mytestboard");
-
-
     }
 
     @Test
     public void createLable() {
-        String lableName = new String("LenaNewLable"+new Date().getTime());
+        String lableName = new String("LenaNewLable" + new Date().getTime());
         pages.boards.createLable(lableName);
         String boardContent = driver.findElement(By.cssSelector(".board-canvas")).getText();
         Assert.assertTrue(boardContent.contains(lableName));
